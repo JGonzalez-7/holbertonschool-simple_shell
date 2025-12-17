6a2d0cce1ce30266e17ed221f4fa674ca91f94d5
Joshua <joshgonzalez027@gmail.com>
2025-12-17 18:58:08 -0400

    Adding main.c file for the main loop of the project

---
+++ main_shell.c
@@ -0,0 +1,44 @@
+#include "shell.h"
+
+/**
+ * main - Entry point of the simple shell program
+ *
+ * Description:
+ * This function runs the main loop of the shell. It continuously
+ * displays a prompt, reads user input from standard input, parses
+ * the command line into arguments, checks for built-in commands,
+ * and executes external commands using fork and execve.
+ * The loop only stops when an end-of-file condition (Ctrl + D)
+ * is encountered.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char *line;
+	char **args;
+
+	while (1)
+	{
+		print_prompt();
+		line = read_line();
+
+		if (line == NULL)
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		args = parse_line(line);
+		if (args && args[0])
+		{
+			if (!handle_builtin(args))
+				execute_command(args);
+		}
+
+		free(line);
+		free(args);
+	}
+	return (0);
+}
