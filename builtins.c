8b6f1d734ab65d92815ec1b9fb48e41313647980
Joshua <joshgonzalez027@gmail.com>
2026-01-01 20:47:08 -0400

    Now the builtin_exit accepts an optional status argument or defaults to last status (lines 13–20); builtin_env returns 0 success (lines 32–42)

--- builtins.c
+++ builtins.c
@@ -3,16 +3,21 @@
 /**
  * builtin_exit - Exits the shell
  * @args: Array of arguments (unused)
+ * @last_status: Last command status to use when no argument is provided
  *
  * Description: Implements the exit built-in command.
  * Exits the shell with status 0.
  *
  * Return: Does not return (exits the program)
  */
-int builtin_exit(char **args)
+void builtin_exit(char **args, int last_status)
 {
-	(void)args;
-	exit(0);
+	int status = last_status;
+
+	if (args[1] != NULL)
+		status = atoi(args[1]);
+
+	exit(status);
 }
 
 /**
@@ -22,7 +27,7 @@ int builtin_exit(char **args)
  * Description: Implements the env built-in command.
  * Prints all environment variables to stdout.
  *
- * Return: 1 to continue the shell loop
+ * Return: 0 to indicate success
  */
 int builtin_env(char **args)
 {
@@ -34,5 +39,5 @@ int builtin_env(char **args)
 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 	}
-	return (1);
+	return (0);
 }
